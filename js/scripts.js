a7ce6aa67ca5ed16af665a0cab91c8c30edb7bee
Daria Sizeva <92261857+dariasi@users.noreply.github.com>
2021-10-11 23:20:23 +0300

    Добавлен выбор мест в кинотеатре, скрипт js
    Подсчет суммы выбранных билетов

---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+const schemeSvg = document.querySelector('.scheme-svg');
+const totalPriceTag=document.querySelector(".price-total");
+let cost=350 ;
+let totalPrice=0;
+schemeSvg.addEventListener("click",(event) =>{ 
+    if(!event.target.classList.contains('booked')){
+       event.target.classList.toggle('active');
+       let totalSeats=schemeSvg.querySelectorAll('.active').length;
+       totalPrice= totalSeats* cost;
+       totalPriceTag.textContent=totalPrice;
+     
+    }
+
+})
+
+/*
+const sessionDateBlock = document.querySelector('.session-date-item');
+sessionDateBlock.addEventListener("click",(event) =>{ 
+    console.log("приветики");
+
+     if(!event.target.classList.contains('active')){
+       event.target.classList.toggle('active');
+     }
+})
+*/
